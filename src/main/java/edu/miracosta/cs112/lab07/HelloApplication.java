aa88a39df7994d37ecf93f1344bf1ee4fe4b074e
MCCLab4804WD28 <clairekingthequeen@gmail.com>
2024-12-02 13:27:04 -0800

    Did part 1 & 2

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,8 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
 import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
@@ -12,14 +14,35 @@ import javafx.event.ActionEvent;        //class for type of event for action (li
 public class HelloApplication extends Application  { //inheriting core functionality + this class will handle events
     /*** GUI COMPONENTS ***/
     // TODO: follow step 25 in README.md to create reference variables
-
     /*** DRIVER main ***/
     public static void main(String[] args) {
         launch(args); //method from Application class, must be called to setup javafx application
     }
 
     // TODO: follow steps 2-9 in README.md to create a start method
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hell GUI: Claire");
+        StackPane layout = new StackPane();
 
+        Label label = new Label("Hello GUI World");
+        Label l2 = new Label("Label 2");
+        Label l3 = new Label("Label 3");
+
+        AnchorPane.setBottomAnchor(label,0.0);
+        AnchorPane.setRightAnchor(label,0.0);
+
+        layout.getChildren().add(label);
+        layout.getChildren().add(l2);
+        layout.getChildren().add(l3);
+
+
+        Scene scene = new Scene(layout, 300, 300);
+        stage.setScene(scene);
+
+
+
+        stage.show();
+    }
     // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
 
     // TODO: follow steps 22-34 in README.md to create an event handler
